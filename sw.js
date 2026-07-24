5518b769294051b2d91113153d20dfaae0b44c12
keifree <47268070+keifree@users.noreply.github.com>
2026-07-24 21:50:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Caches core app assets for offline launch & satisfies PWA installation criteria.
  */
 
-const CACHE_NAME = 'pdf-studio-v80';
+const CACHE_NAME = 'pdf-studio-v90';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
